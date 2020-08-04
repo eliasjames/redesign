f7b14d36efed7fdf8f2041d6f52d630502871b76
Elias Carlston <elias@eliascarlston.com>
2020-08-04 12:05:11 -0400

    writing files

---
+++ elias-judas-writing.js
@@ -0,0 +1,27 @@
+const articles = [
+  {
+    "category": "life and love",
+    "link": "https://medium.com/@eliascarlston/your-choice-eb736b684b6c",
+    "shortName": "Your Choice"
+  },
+  {
+    "category": "lyrics",
+    "link": "https://medium.com/@eliascarlston/ideal-eyes-aa7f6695fa9",
+    "shortName": "Ideal Eyes"
+  },
+  {
+    "category": "opinion",
+    "link": "https://medium.com/@eliasjudas/this-earth-day-lets-do-little-aae25b01e0e9",
+    "shortName": "Earth Day"
+  },
+  {
+    "category": "technical",
+    "link": "https://medium.com/@eliascarlston/a-beginner-s-guide-to-debugging-243f09546f34",
+    "shortName": "A Beginner’s Guide to Debugging"
+  },
+  {
+    "category": "music",
+    "link": "https://medium.com/@eliasjudas/top-records-the-dealer-51797722eea3",
+    "shortName": "My Top Records: Chico Hamilton’s 'The Dealer'"
+  }
+];
